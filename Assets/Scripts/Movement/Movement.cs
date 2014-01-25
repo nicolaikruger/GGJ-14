33a08ef6c1a560230d882477d20885e37ffce733
kasraman1000 <minutegreywing@gmail.com>
2014-01-25 04:35:02 +0100

    Networking is working, no pun intended

---
+++ Assets/Scripts/Movement/Movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 5f;
+
+	// Update is called once per frame
+	void Update () {
+		// only let the owner move this object
+		if (networkView.isMine) {
+			if (Input.GetKey(KeyCode.W)) {
+				transform.Translate(Vector3.forward * Time.deltaTime * speed);
+			}
+			if (Input.GetKey(KeyCode.S)) {
+				transform.Translate(Vector3.back * Time.deltaTime * speed);
+			}
+			if (Input.GetKey(KeyCode.A)) {
+				transform.Translate(Vector3.left * Time.deltaTime * speed);
+			}
+			if (Input.GetKey(KeyCode.D)) {
+				transform.Translate(Vector3.right * Time.deltaTime * speed);
+			}
+		}
+		else enabled = false;
+	}
+}
---
+++ Assets/Scripts/Networking/NetworkManagerScript.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManagerScript : MonoBehaviour {
+
+	// Inspector vars
+	public int maxConnections = 4;
+	public int port = 25002;
+	public string gameNameForMasterServer = "GGJ14_Game_CPH";
+	public string gameName = "GGJ14 Game server!";
+	public string comment = "";
+
+	public Transform spawnPoint;
+	public GameObject playerPrefab;
+
+	// Server list
+	private HostData[] hosts;
+	private bool refreshing = false;
+
+	// button size whatevs
+	private float btnX;
+	private float btnY;
+	private float btnW;
+	private float btnH;
+
+	// Called on instantiate
+	void Start() {
+		btnX = Screen.width * 0.05f;
+		btnY = Screen.height * 0.05f;
+		btnW = Screen.width * 0.2f;
+		btnH = Screen.width * 0.1f;
+	}
+
+	//
+	// Networking shit up here
+	//
+
+	void StartServer() {
+		Network.InitializeServer(maxConnections, port, !Network.HavePublicAddress());
+
+	}
+
+	// ask master server for list of games
+	void RefreshHostList() {
+		refreshing = true;
+		MasterServer.RequestHostList(gameNameForMasterServer);
+	}
+
+	// Spawn the player at the spawn point object
+	void SpawnPlayer() {
+		Network.Instantiate(playerPrefab, spawnPoint.position, Quaternion.identity, 0);
+	}
+
+
+	//
+	// Event shit here
+	//
+
+	// Called when server is initialized
+	void OnServerInitialized() {
+		Debug.Log("Server initialized! Broadcasting to master server...");
+		MasterServer.RegisterHost(gameNameForMasterServer, gameName, comment);
+		SpawnPlayer();
+	}
+
+	void OnConnectedToServer() {
+		Debug.Log("Connected to server!");
+		SpawnPlayer();
+	}
+
+	// Called when master server shit happens
+	void OnMasterServerEvent(MasterServerEvent mse) {
+		// When registration happens
+		if (mse == MasterServerEvent.RegistrationSucceeded) {
+			Debug.Log("Server registered on Master server!");
+		}
+
+		// When server list is recieved, store it
+		if (mse == MasterServerEvent.HostListReceived) {
+			hosts = MasterServer.PollHostList();
+			refreshing = false;
+		}
+	}
+
+
+
+
+	//
+	// GUI shit down here
+	//
+
+	// Called with each GUI event (multiple times per frame sometimes)
+	void OnGUI() {
+		if (GUI.Button(new Rect(btnX, btnY, btnW, btnH), "Start server")) {
+			Debug.Log ("Starting Server");
+			StartServer();
+		}
+		if (GUI.Button(new Rect(btnX, btnY * 1.2f + btnH, btnW, btnH), "Refresh server list")) {
+			Debug.Log ("Refreshing server list...");
+			RefreshHostList();
+		}
+		if (hosts != null) {
+			for (int i = 0; i < hosts.Length; i++) {
+				// put out a button for each server, clicking it will connect to server
+				if (
+				GUI.Button(
+					new Rect(btnX * 1.5f + btnW, btnY * 1.2f + (btnH * i), btnW*3f, btnH), 
+					hosts[i].ip[0] + " - " + hosts[i].connectedPlayers + " players")
+				) {
+					Network.Connect(hosts[i]);
+				}
+			}
+		}
+	}
+}
